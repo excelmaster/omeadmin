1f39c0cd91b3b7df7bdd33b7c9abb481fc4bd430
betfie1@gmail.com <betfie1@gmail.com>
2022-11-17 18:12:33 -0500

    default enrouting to courses

--- manager/app/Config/Routes.php
+++ manager/app/Config/Routes.php
@@ -17,7 +17,7 @@ if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Course');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -35,7 +35,7 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Course::index');
 $routes->get('/courses', 'Course::index');
 $routes->get('/lessons/list/(:num)/(:any)','Lesson::index/$1/$2');
 $routes->get('/activities/list/(:num)/(:num)','Activity::index/$1/$2');
