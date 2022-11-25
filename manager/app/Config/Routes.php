f700fcb2681b54e626bb68b56d91ba16d6d02fa7
root <root@server1.mundoeducativodigital.com>
2022-11-25 08:34:09 -0500

    fix routes to login

--- manager/app/Config/Routes.php
+++ manager/app/Config/Routes.php
@@ -51,9 +51,9 @@ $routes->group('auth', ['namespace' => 'IonAuth\Controllers'], function ($routes
 	$routes->add('login', 'Auth::login');
 	$routes->get('logout', 'Auth::logout');
 	$routes->add('forgot_password', 'Auth::forgot_password');
-	// $routes->get('/', 'Auth::index');
+	$routes->get('/', 'Auth::index');
 	$routes->add('create_user', 'Auth::create_user');
-	// $routes->add('edit_user/(:num)', 'Auth::edit_user/$1');
+	$routes->add('edit_user/(:num)', 'Auth::edit_user/$1');
 	// $routes->add('create_group', 'Auth::create_group');
 	// $routes->get('activate/(:num)', 'Auth::activate/$1');
 	// $routes->get('activate/(:num)/(:hash)', 'Auth::activate/$1/$2');
