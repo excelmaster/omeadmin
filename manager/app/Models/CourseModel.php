861bf40352a9a478e29969c977a30d1d22f3f59f
excelmaster <betfie1@gmail.com>
2023-05-09 12:36:28 -0500

    change settings to display all courses (deleted and not deleted)

--- manager/app/Models/CourseModel.php
+++ manager/app/Models/CourseModel.php
@@ -21,7 +21,7 @@ class CourseModel extends Model
     protected $skipValidation     = false;
 
     public function readCourses(){
-        return $this->orderBy('category', 'asc')->findAll();        
+        return $this->orderBy('category', 'asc')->withDeleted()->findAll();        
     }
 
 }
